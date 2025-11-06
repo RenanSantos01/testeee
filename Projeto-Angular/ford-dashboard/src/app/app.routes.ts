e447ace2c5860a9b8b4b8757ba3293ef5a2c3ed5
Renan Santos <88851654+RenanSantos01@users.noreply.github.com>
2025-11-06 16:35:30 -0300

    UPDATE

--- Projeto-Angular/ford-dashboard/src/app/app.routes.ts
+++ Projeto-Angular/ford-dashboard/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
-// Caso tenha uma tela "Home" depois do login:
-//import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'; // ✅ importe o componente Home
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  //{ path: 'home', component: HomeComponent } // opcional
+  { path: 'home', component: HomeComponent } // ✅ rota ativa
 ];
